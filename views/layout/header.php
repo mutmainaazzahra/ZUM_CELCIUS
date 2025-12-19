c52d8bde1d30f84e2b328042fddf3f5c6b507eee
mutmainaazzahra <if24.mutmainaazzahra@mhs.ubpkarawang.ac.id>
2025-12-19 17:53:55 +0700

    add logo header

--- views/layout/header.php
+++ views/layout/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zum Celcius - Weather & Activity</title>
+    <link rel="icon" type="image/png" href="assets/logo.png">
     <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../node_modules/bootstrap-icons/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
